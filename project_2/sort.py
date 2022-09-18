dbd0799c175fb6ba0546b9bf8fc7a46c8db29f62
Jared Mundt <jaredmundt@gmail.com>
2022-09-18 15:19:57 -0600

    project 2 sorting

--- project_2/sort.py
+++ project_2/sort.py
@@ -7,9 +7,17 @@ import random
 import time
 
 
+def main():
+    random.seed(111)
+    max_num = 1000000
+    array_len = 10000
+    nums: list[int] = random.sample(range(max_num), k=array_len)
+    print_results(nums)
+
+
 def time_function(run_f, args):
     start = time.perf_counter()
-    print(is_sorted(run_f(args)))
+    run_f(args)
     f_time = time.perf_counter() - start
     return f_time
 
@@ -32,37 +40,78 @@ def print_results(nums):
     print(f"timsort duration: {f_time} seconds.")
 
 
-def main():
-    random.seed(1111)
-    max_num = 10000000
-    array_len = 10000
-    nums: list[int] = random.sample(range(max_num), k=array_len)
-    print_results(nums)
-
-
 def quicksort(nums: list[int]) -> list[int]:
-    nums.sort()
+
+    def quicksort_helper(left, right, nums):
+
+        if left < right:
+            part = partition(left, right, nums)
+            quicksort_helper(left, part - 1, nums)
+            quicksort_helper(part + 1, right, nums)
+
+    def partition(left, right, nums) -> int:
+
+        index = left
+        pivot = nums[right]
+
+        for i in range(left, right):
+            if nums[i] <= pivot:
+                nums[i], nums[index] = nums[index], nums[i]
+                index += 1
+        nums[right], nums[index] = nums[index], nums[right]
+        return index
+
+    quicksort_helper(0, len(nums) - 1, nums)
     return nums
 
 
 def mergesort(nums: list[int]) -> list[int]:
-    nums.sort()
+
+    def mergesort_helper(nums: list[int]) -> list[int]:
+        if len(nums) < 2:
+            return nums
+        half = len(nums) // 2
+        return merge(mergesort_helper(
+            nums[:half]), mergesort_helper(nums[half:]))
+
+    def merge(nums1: list[int], nums2: list[int]) -> list[int]:
+        res: list[int] = []
+        i = 0
+        j = 0
+
+        while i < len(nums1) or j < len(nums2):
+            if i >= len(nums1):
+                res.append(nums2[j])
+                j += 1
+            elif j >= len(nums2):
+                res.append(nums1[i])
+                i += 1
+            elif nums1[i] < nums2[j]:
+                res.append(nums1[i])
+                i += 1
+            else:
+                res.append(nums2[j])
+                j += 1
+
+        return res
+
+    nums = mergesort_helper(nums)
     return nums
 
 
 def selection_sort(nums: list[int]) -> list[int]:
-    for i in range(0, len(nums)):
-        for j in range(i+1, len(nums)):
+    for i, _ in enumerate(nums):
+        for j in range(i + 1, len(nums)):
             if nums[i] > nums[j]:
                 nums[i], nums[j] = nums[j], nums[i]
     return nums
 
 
 def insertion_sort(nums: list[int]) -> list[int]:
-    for i in range(0, len(nums)):
+    for i, _ in enumerate(nums):
         j = i
-        while j > 0 and nums[j] < nums[j-1]:
-            nums[j-1], nums[j] = nums[j], nums[j-1]
+        while j > 0 and nums[j] < nums[j - 1]:
+            nums[j - 1], nums[j] = nums[j], nums[j - 1]
             j -= 1
 
     return nums
@@ -74,7 +123,7 @@ def timsort(nums: list[int]) -> list[int]:
 
 
 def is_sorted(nums) -> bool:
-    if (all(nums[i] <= nums[i + 1] for i in range(len(nums) - 1))):
+    if all(nums[i] <= nums[i + 1] for i in range(len(nums) - 1)):
         return True
     return False
 
